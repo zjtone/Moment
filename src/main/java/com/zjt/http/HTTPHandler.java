f5d8364575c1bd90627329e27c99264c6d196f5d
zjtone <13682427795@163.com>
2018-06-04 19:41:43 +0800

    非正式提交

--- src/main/java/com/zjt/http/HTTPHandler.java
+++ src/main/java/com/zjt/http/HTTPHandler.java
@@ -16,44 +16,28 @@ public class HTTPHandler implements IHandler {
                 int length = 0, read_len = -1;
                 int totalLength = inputStream.available();
                 if(totalLength <= 0)continue;
-                byte[] read = new byte[totalLength];
-                while(length < totalLength && 
-                    (read_len = inputStream.read(read, length, totalLength - length)) > 0) {
-                    length += read_len;
+                HTTPRequest request = new HTTPRequest(inputStream);
+                HTTPResponse response = new HTTPResponse(outputStream);
+                Api api;
+                if(request.method().toLowerCase().equals("get"))
+                    api = Dispatcher.getInstance().get(request.uri());
+                else{
+                    api = Dispatcher.getInstance().post(request.uri());
                 }
-                if(length > 0){
-                    System.out.println("" + new String(read));
-                    HTTPRequest request = new HTTPRequest(read);
-                    HTTPResponse response = new HTTPResponse(outputStream);
-                    Api api;
-                    if(request.method().toLowerCase().equals("get"))
-                        api = Dispatcher.getInstance().get(request.uri());
-                    else api = Dispatcher.getInstance().post(request.uri());
-                    if(api == null && mDefaultApi != null){
-                        mDefaultApi.response(request, response);
-                    }
-                    if(api != null)
-                        api.response(request, response);
-                    break;
+                if(api == null && mDefaultApi != null){
+                    mDefaultApi.response(request, response);
                 }
-            }catch(IOException e){
+                if(api != null)
+                    api.response(request, response);
+                break;
+            }catch(IOException e) {
                 //  break;
+                e.printStackTrace();
             }
         }
         inputStream.close();
         outputStream.close();
         socket.close();
-    }
-
-    private int headerDivider(byte[] b, int offset) {
-        int location = -1;
-        for(int i = offset ; i + 4 < b.length ; i++) {
-            if(b[i] == 13 && b[i + 1] == 10 && b[i + 2] == 13 && b[i + 3] == 10) {
-                location = i;
-                break;
-            }
-        }
-        return location;
     }
 
     public HTTPHandler defaultApi(Api api){
--- src/main/java/com/zjt/http/HTTPRequest.java
+++ src/main/java/com/zjt/http/HTTPRequest.java
@@ -1,48 +1,19 @@
 package com.zjt.http;
 
 import java.util.HashMap;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class HTTPRequest {
     private HashMap<String, String> mHashMap;
     private String method = "", uri = "";
-    private byte[] mContent;
+    private byte[] data;
     private boolean isGzip, isKeepAlive;
 
-    public HTTPRequest(){
+    public HTTPRequest(InputStream inputStream){
         mHashMap = new HashMap<>();
-    }
-
-    public HTTPRequest(byte[] b){
-        this(new String(b));
-    }
-
-    public HTTPRequest(String header){
-        mHashMap = new HashMap<>();
-        int index = header.indexOf("\r\n\r\n");
-        header = header.substring(0, index + 1);
-        index = header.indexOf(" ");
-        // 请求的方法
-        method = header.substring(0, index);
-        // 请求的资源
-        int newIndex = header.indexOf(" ", index + 1);
-        uri = header.substring(index + 1, newIndex);
-        header = header.substring(header.indexOf("\r\n", newIndex + 1) + 2);
-        // 头部其他字段
-        String[] headers = header.split("\r\n");
-        if(headers != null){
-            for(String h: headers){
-                String[] str = h.split(":");
-                if(str != null){
-                    mHashMap.put(str[0].trim(), str[1].trim());
-                    if(str[0].equalsIgnoreCase("Accept-Encoding") && str[1].contains("gzip")){
-                        isGzip = true;
-                    }
-                    if(str[0].equalsIgnoreCase("Connection") && str[1].equalsIgnoreCase("keep-alive")){
-                        isKeepAlive = true;
-                    }
-                }
-            }
-        }
+        byte[] read = new byte[2048];
+        
     }
 
     public boolean gzip(){
@@ -51,10 +22,6 @@ public class HTTPRequest {
 
     public boolean keepAlive(){
         return isKeepAlive;
-    }
-
-    public byte[] content(){
-        return mContent;
     }
 
     public String get(String key){
