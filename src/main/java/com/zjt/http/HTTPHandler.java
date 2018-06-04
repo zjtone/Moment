e91ce0db69d3921ba5e5900fca2786ed97bc3862
zjtone <13682427795@163.com>
2018-06-04 18:42:36 +0800

    减少了内存占用

--- src/main/java/com/zjt/http/HTTPHandler.java
+++ src/main/java/com/zjt/http/HTTPHandler.java
@@ -7,20 +7,22 @@ import com.zjt.handler.*;
 public class HTTPHandler implements IHandler {
     private Api mDefaultApi;
 
-    public void read(Socket socket) throws IOException{
-        int totalLength = 1024 * 1024 * 2;
-        byte[] read = new byte[totalLength];
+    public void read(Socket socket) throws IOException {
         InputStream inputStream = socket.getInputStream();
         OutputStream outputStream = socket.getOutputStream();
         long startTime = System.currentTimeMillis();
-        while(!socket.isClosed() && startTime + 10000 > System.currentTimeMillis()){
-            try{
+        while(!socket.isClosed() && startTime + 10000 > System.currentTimeMillis()) {
+            try {
                 int length = 0, read_len = -1;
-                while(length < totalLength && inputStream.available() > 0 && 
-                    (read_len = inputStream.read(read, length, totalLength - length)) > 0){
+                int totalLength = inputStream.available();
+                if(totalLength <= 0)continue;
+                byte[] read = new byte[totalLength];
+                while(length < totalLength && 
+                    (read_len = inputStream.read(read, length, totalLength - length)) > 0) {
                     length += read_len;
                 }
                 if(length > 0){
+                    System.out.println("" + new String(read));
                     HTTPRequest request = new HTTPRequest(read);
                     HTTPResponse response = new HTTPResponse(outputStream);
                     Api api;
@@ -41,6 +43,17 @@ public class HTTPHandler implements IHandler {
         inputStream.close();
         outputStream.close();
         socket.close();
+    }
+
+    private int headerDivider(byte[] b, int offset) {
+        int location = -1;
+        for(int i = offset ; i + 4 < b.length ; i++) {
+            if(b[i] == 13 && b[i + 1] == 10 && b[i + 2] == 13 && b[i + 3] == 10) {
+                location = i;
+                break;
+            }
+        }
+        return location;
     }
 
     public HTTPHandler defaultApi(Api api){
